aa25d8cb092742d4cb14fa8b5778f5a4190ebb8c
Giorgio <giorgio.pignattari@gmail.it>
2021-02-26 16:36:50 +0100

    js ok

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// INPUT UTENTE
+// nome
+var nome = prompt("inserisci il tuo nome")
+// cognome
+var cognome = prompt("inserisci il tuo cognome")
+// colore
+var colore = prompt("inserisci il tuo colore preferito")
+
+//HTML
+// nome
+document.getElementById("nome").innerHTML = nome
+// cognome
+document.getElementById("cognome").innerHTML = cognome
+// colore
+document.getElementById("colore").innerHTML = colore
+
+// password
+document.getElementById("stamp").innerHTML = nome + cognome + colore + "2021"
